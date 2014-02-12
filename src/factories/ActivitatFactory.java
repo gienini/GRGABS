e51b642665db5dd2c2d24c6bcfa856ff524be1d8
fgarcia <francescgmartin@gmail.com>
2014-02-12 16:48:34 +0000

    Factorys tested

--- src/factories/ActivitatFactory.java
+++ src/factories/ActivitatFactory.java
@@ -2,12 +2,13 @@ package factories;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.Calendar;
 
 import beans.Activitat;
 
 public class ActivitatFactory {
 	
-	public Activitat creaActivitat(String nom, String descripcio, Date dia, Timestamp hora, String espai, boolean senior) {
+	public Activitat creaActivitat(String nom, String descripcio, Calendar dia, Timestamp hora, String espai, boolean senior) {
 		return new Activitat(nom,descripcio,dia,hora,espai,senior);
 	}
 	{
